c8f4e6c0c067546e86fbd3ff90bc7962773852c2
igorgp06 <contateme.igorgp@gmail.com>
2025-01-20 12:00:01 -0300

    [WORK 1.2] Criação base do header, implementação lógica do menu com js e adição das outras páginas.

---
+++ assets/js/index/header.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.querySelector('.menu-icon');
+    const closeIcon = document.querySelector('.close-icon');
+    const menu = document.getElementById('menu');
+    const menuLinks = document.querySelectorAll('.navbar-list li a');
+
+    menuIcon.addEventListener('click', function () {
+        menu.classList.add('open')
+    });
+
+    closeIcon.addEventListener('click', function () {
+        menu.classList.remove('open')
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            menu.classList.remove('open')
+        });
+    });
+});
