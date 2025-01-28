86c726271431531ffd539903d4cf0df17170cfcd
igorgp06 <contateme.igorgp@gmail.com>
2025-01-28 20:13:56 -0300

    [WORK 4.6] Alteração das configurações do mapa e mudanças na sua estilização.

--- assets/js/index/map.js
+++ assets/js/index/map.js
@@ -1,8 +1,27 @@
-// Inicializando o mapa
-const map = L.map('map').setView([-14.2350, -51.9253], 4);
+const map = L.map('map', {
+    center: [-15.084058, -53.481445],
+    zoom: 4,
+    maxBounds: [
+        [-33.8688, -73.9828],
+        [5.2718, -34.7297]
+    ],
+    maxZoom: 8,
+    minZoom: 4
+});
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors and <a href="https://igorgp06.github.io/DestinosDoKite/" target="_blank">Destinos do Kite</a>',
+/* 
+
+por no tileLayer e nas atribuições a depender da decisão do cliente se prefero com ou sem
+
+https://tile.openstreetmap.org/{z}/{x}/{y}.png
+
+'&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="external">OpenStreetMap</a> 
+contributors and <a href="https://igorgp06.github.io/DestinosDoKite/">Destinos do Kite</a>'
+
+*/
+
+L.tileLayer('', {
+    attribution: '&copy; <a href="https://igorgp06.github.io/DestinosDoKite/">Destinos do Kite</a>',
 }).addTo(map);
 
 fetch('../../assets/static/geojson/brazil-states.geojson')
@@ -11,17 +30,32 @@ fetch('../../assets/static/geojson/brazil-states.geojson')
         L.geoJSON(data, {
             style: function (feature) {
                 return {
-                    color: "#000",
-                    weight: 1,
-                    fillColor: "#FFF",
-                    fillOpacity: 0.1 
+                    color: "#4e5873",
+                    weight: 2,
+                    fillColor: "#797f8d",
+                    fillOpacity: 0.3
                 };
-            },
-            onEachFeature: function (feature, layer) {
-
-                layer.on('click', function () {
-                    alert(`Você clicou no estado: ${feature.properties.NAME}`);
-                });
             },
         }).addTo(map);
     });
+
+// Marcadores de teste
+L.marker([-27.89037, -49.119873]).addTo(map)
+    .bindPopup('<a href="../../assets/html/pages/states.html">Clique para ver as escolas de <br>kitesurf presentes em Santa Catarina</br></a>');
+
+L.marker([-24.57991, -51.800537]).addTo(map)
+    .bindPopup('<a href="../../assets/html/pages/states.html">Clique para ver as escolas de <br>kitesurf presentes no Paraná</br></a>');
+
+/* 
+
+função para pegar as coordenadas do click
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
+
+*/
+
+
