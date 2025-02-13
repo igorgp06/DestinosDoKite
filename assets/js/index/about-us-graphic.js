0b341b313cd14d1eef89ed4c95203b228deacf24
igorgp06 <igorgoncalvesp20@gmail.com>
2025-02-12 22:54:18 -0300

    [WORK 7.1] Adição do AOS na página quem somos, melhorias no gráfico e etc.

--- assets/js/index/about-us-graphic.js
+++ assets/js/index/about-us-graphic.js
@@ -7,34 +7,69 @@ document.addEventListener("DOMContentLoaded", function () {
                 show: false,
             },
         },
-        series: [
-            {
-            name: 'Estatísticas',
-            data: [1054526, 1014237, 1514657],
-        }],
-        xaxis: {
-            categories: ['Contas Alcançadas', 'Visualizações', 'Impressões'],
-        },
-        colors: ['#797f8d'],
         plotOptions: {
             bar: {
-                horizontal: true,
-                borderRadius: 4,
+                barHeight: '100%',
+                distributed: false,
+                dataLabels: {
+                    position: 'bottom'
+                }
+            }
+        },
+        series: [{
+            data: [{
+                x: 'Contas Alcançadas',
+                y: 1054526
             },
+            {
+                x: 'Visualizações',
+                y: 1012100
+            },
+            {
+                x: 'Impressões',
+                y: 1514657
+            },
+            {
+                x: 'Novos Seguidores',
+                y: 61030
+            },
+            ]
+        }],
+        xaxis: {
+            categories: ['Contas Alcançadas', 'Visualizações', 'Impressões', 'Novos Seguidores'],
+        },
+        yaxis: {
+            labels: {
+                show: false
+            }
+        },
+        colors: ['#4e5873', '#6c778c', '#797f8d', '#bfc7d9'],
+        stroke: {
+            show: true,
+            width: 1,
+            colors: ['#bfc7d9', '#797f8d', '#6c778c', '#4e5873']
         },
         dataLabels: {
             enabled: true,
+            offsetY: 50,
+            style: {
+                fontSize: '13px',
+                colors: ['black']
+            }
         },
         title: {
-            text: 'Estatísticas do Instagram',
+            text: 'Estatísticas do Instagram nos últimos 90 dias',
             align: 'center',
+            floating: true,
             style: {
-                fontSize: '18px',
+                fontSize: '20px',
                 fontWeight: 'bold',
-            },
-        }
+                color: '#4e5873'
+            }
+        },
     };
 
-    const chart = new ApexCharts(document.querySelector("#chart"), options);
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
+
 });
