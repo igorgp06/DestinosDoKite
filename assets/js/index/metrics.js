c9c3a12a9c5c434051e05df755f8c8a5f21b75f8
igorgp06 <igorgoncalvesp20@gmail.com>
2025-02-14 16:19:32 -0300

    [WORK] Melhorias na estilização da página "Quem Somos" e um novo visual para o gráfico.

--- assets/js/index/metrics.js
+++ assets/js/index/metrics.js
@@ -17,30 +17,26 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         },
         series: [{
-            data: [{
-                x: 'Contas Alcançadas',
-                y: 1054526
-            },
-            {
-                x: 'Visualizações',
-                y: 1012100
-            },
-            {
-                x: 'Impressões',
-                y: 1514657
-            },
-            {
-                x: 'Novos Seguidores',
-                y: 61030
-            },
+            name: 'Valores',
+            data: [
+                { x: 'Contas Alcançadas', y: 1054526 },
+                { x: 'Visualizações', y: 1012100 },
+                { x: 'Impressões', y: 1514657 },
+                { x: 'Novos Seguidores', y: 6130 }
             ]
         }],
         xaxis: {
             categories: ['Contas Alcançadas', 'Visualizações', 'Impressões', 'Novos Seguidores'],
+            labels: {
+                style: {
+                    colors: '#000'
+                }
+            },
+
         },
         yaxis: {
             labels: {
-                show: false
+                show: false,
             }
         },
         colors: ['#4e5873', '#6c778c', '#797f8d', '#bfc7d9'],
@@ -52,6 +48,13 @@ document.addEventListener("DOMContentLoaded", function () {
         dataLabels: {
             enabled: true,
             offsetY: 50,
+            formatter: function (val) {
+                if (val >= 1000) {
+                    return (val / 1000).toFixed(1).replace('.', ',') + 'k';
+                } else {
+                    return val.toString().replace('.', ',');
+                }
+            },
             style: {
                 fontSize: '13px',
                 colors: ['black']
@@ -67,6 +70,25 @@ document.addEventListener("DOMContentLoaded", function () {
                 color: '#4e5873'
             }
         },
+        tooltip: {
+            y: {
+                formatter: function (val) {
+                    return val.toLocaleString('pt-BR');
+                }
+            },
+            marker: {
+                show: false
+            },
+            custom: function ({ series, seriesIndex, dataPointIndex, w }) {
+                const data = w.config.series[seriesIndex].data[dataPointIndex];
+                return `
+                    <div class="custom-tooltip">
+                        <strong>${data.x}</strong><br>
+                        Valor: ${data.y.toLocaleString('pt-BR')}
+                    </div>
+                `;
+            }
+        }
     };
 
     var chart = new ApexCharts(document.querySelector("#chart"), options);
